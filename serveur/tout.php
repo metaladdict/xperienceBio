b5c70eb84c88d94b788947e3ee87522c84158a1e
Erwan K <gitlab@wdev.pro>
2021-03-29 14:46:15 +0200

    Pas de listing des répertoires

--- serveur/tout.php
+++ serveur/tout.php
@@ -18,7 +18,7 @@ $f = scandir('./sauvegarde/thb');
 
 foreach($f as $v)
 {
-  if($v!='..' && $v!='.')
+  if($v!='..' && $v!='.' && $v!='index.htm')
   {
     $exp = explode('.', $v);
     echo '<tr><th>'.strftime('%d/%m/%Y<br>%H:%M:%S', str_replace('pix_', '', $exp[0])).'</th><td><img src="sauvegarde/thb/'.$v.'"></td></tr>';
