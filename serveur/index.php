b7f0ee61d582751c2545cfbe8b3ffd1520fd9273
Erwan K <gitlab@wdev.pro>
2021-04-01 00:31:25 +0200

    ajout du temps du timelapse

--- serveur/index.php
+++ serveur/index.php
@@ -7,18 +7,43 @@
   <title>xPerience !</title>
   <style>
     img, video {display: block; max-width: 100%; height: auto;}  
+    h1 {margin-bottom: 5px;}
   </style>
 </head>
 <body>
 <?php
 $fld = 'sauvegarde/src/';
 $files = scandir($fld, SCANDIR_SORT_DESCENDING);
+array_pop($files);
+array_pop($files);
+array_pop($files);
+
+$iz = str_replace(['.jpg', 'pix_'], '', $files[0]);
+$ia = str_replace(['.jpg', 'pix_'], '', end($files));
+$iv = filemtime('./sauvegarde/lavideo.mp4');
+
 echo '<a href="tout.php" style="float: right;">Toutes les images</a>';
-echo '<h1>Dernière vue - '.strftime('%d/%m/%Y à %H:%M:%S', str_replace(['.jpg', 'pix_'], '', $files[0])).'</h1>';
+echo '<h1>Dernière vue - '.strftime('%d/%m/%Y à %H:%M:%S', $iz).'</h1>';
 echo '<img src="'.$fld.$files[0].'">';
 
-echo '<h1>Timelapse - '.strftime('%d/%m/%Y à %H:%M:%S', filemtime('./sauvegarde/lavideo.mp4')).'</h1>';
+echo '<h1>Timelapse sur '.readableTime($iz-$ia).'</h1>';
+echo '<em>publié il y a '.readableTime(time() - $iv).'</em>';
 echo '<video src="sauvegarde/lavideo.mp4" controls></video>';
+
+function readableTime($t)
+{
+  $tj = floor($t/(3600*24));
+  $th = floor(($t-$tj*(3600*24))/3600);
+  $tm = floor(($t-$tj*(3600*24)-$th*3600)/60);
+  $ts = $t-$tj*(3600*24)-$th*3600-$tm*60;
+
+  $txt = ($tj>0) ? $tj.'j ' : '';
+  $txt.= ($th>0 && $t<10*3600*24) ? $th.'h ' : '';
+  $txt.= ($tm>0 && $t<24*3600*10) ? $tm.'min. ' : '';
+  $txt.= ($ts>0 && $t<36000) ? $ts.'sec. ' : '';
+
+  return $txt;
+}
 
 ?>
 <br><a href="https://www.metalmonkey.eu/2021/03/27/suivi-de-la-germination-des-lentilles/" target="_blank">Comment ça marche ?</a>
